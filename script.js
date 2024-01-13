2377dbfb39813ca5ecdb046ba85141c098e937f4
mahdiakil <mahdiakil@live.com>
2024-01-13 17:00:07 +0100

    logo fixed

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ document.querySelectorAll('.nav-links a[href^="#"]').forEach(anchor => {
 document.querySelector('.hamburger-menu').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('nav-active');
 });
+document.getElementById('logoLink').addEventListener('click', function(e) {
+    e.preventDefault(); // Prevents the default anchor behavior
+    window.scrollTo(0, 0); // Scrolls to the top of the page
+});
